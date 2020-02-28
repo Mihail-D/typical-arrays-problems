335159b0a4cde137b80c576bc61a5d11a6eec482
Mihail-D <nuby.4register@gmail.com>
2020-02-28 09:59:42 +0300

    feat: add completed 'Arrays Problem' task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
+function sort(array) {
+    let tmp;
+    let swapped;
 
-exports.min = function min (array) {
-  return 0;
+    do {
+        swapped = false;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > array[i + 1]) {
+                let tmp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = tmp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+
+    return array;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length <= 0) {
+        return 0;
+    } else {
+        sort(array);
+        return array[0];
+    }
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length <= 0) {
+        return 0;
+    } else {
+        sort(array);
+        return array[array.length - 1];
+    }
+};
+
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length <= 0) {
+        return 0;
+    } else {
+        let count = 0;
+        for (const i of array) {
+            count += i;
+        }
+        return count / array.length;
+    }
+};
